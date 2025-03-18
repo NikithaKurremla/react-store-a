4a5c90ad9d75efd5383c48c4dd4d3354d8044961
NikithaKurremla <kurremlanikitha@gmail.com>
2025-03-18 14:21:03 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 import Header from "./components/Header";
 import Products from "./components/Products";
-import Footer from "./components/Footer";
-import Login from "./components/Login";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footers from  "./components/Footers";
+import {BrowserRouter,Route,Routes} from "react-router-dom";
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route index element={<Products />}></Route>
-          <Route path="products" element={<Products />}></Route>
-          <Route path="login" element={<Login />}></Route>
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route index element={<Products />}></Route>
+        <Route path="products" element={<Products />}></Route>
+        <Route path="login" element={<Login />}></Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  </div>
   );
 }
+
 export default App;
+
