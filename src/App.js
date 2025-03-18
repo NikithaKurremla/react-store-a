88c643e0d6e973653790c9db5e55d1a5a2ca5464
NikithaKurremla <kurremlanikitha@gmail.com>
2025-03-18 14:16:12 +0530

    json

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from "./components/Header"
-import Products from "./components/Products"
-import Footers from  "./components/Footers"
+import Header from "./components/Header";
+import Products from "./components/Products";
+import Footers from  "./components/Footers";
+import {BrowserRouter,Route,Routes} from "react-router-dom";
 function App() {
   return (
     <div>
-
+      <BrowserRouter>
       <Header/>
       <Products/>
       <Footers/>
+      <Routes>
+        <Route index element={<Products/>}></Route>
+       <Route path="products" element={<Products/>}></Route>
+       <Route path="login" element={<Login/>}></Route>
+      </Routes>
+      </BrowserRouter>
       
     </div>
   );
 }
 
 export default App;
+
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function Login(){
+export default function Cart(){
     return(
-        <div>Login</div>
+        <div>Cart</div>
     )
 }
